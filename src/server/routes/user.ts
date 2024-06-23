0ef43a4e0a3047d0d73d32e5ec04da9955c45ea8
bikrantjung <bikrantjung1214@gmail.com>
2024-06-23 19:05:38 +0545

    fix(user-api): improve friend request sending api

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -45,7 +45,36 @@ const userRoutes = app
     ),
     async (c) => {
       const { senderId, receiverId } = c.req.valid("json")
+
+      // Cannot send friend request to ourselves
+      if (senderId == receiverId)
+        return c.json({ message: "Cannot send friend request!" }, 400)
+
       try {
+        // Cannot send friend request if already in pending or already friends
+        const sender = await db.user.findUnique({
+          where: {
+            id: senderId,
+          },
+          include: {
+            sentFriendRequests: true,
+            friends: true,
+          },
+        })
+        if (!sender) return c.json({ message: "Cannot find the user" }, 400)
+        const request = sender.sentFriendRequests.find(
+          (request) => request.receiverId === receiverId
+        )
+        if (request) {
+          // The request is already sent
+          return c.json({ message: "Request already sent" }, 400)
+        }
+        const friend = sender.friends.find((friend) => friend.id === receiverId)
+        if (friend) {
+          // the receiver id is already a friend
+          return c.json({ message: "Already a friend" }, 400)
+        }
+
         await db.friendRequest.create({
           data: {
             receiverId,
